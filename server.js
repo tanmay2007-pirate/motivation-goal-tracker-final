7fdf267460ef62b9935294e8e8310538e377a346
Tanmay Patharkar <tanmaypatharkar2007@gmail.com>
2026-03-25 23:54:53 +0530

    connecting frontend

--- server.js
+++ server.js
@@ -23,6 +23,20 @@ app.use(cors({
 }));
 
 app.options("*", cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://motivation-goal-tracker-final.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(express.json());
 app.use(express.static('public'));
 
